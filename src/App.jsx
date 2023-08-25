04703837309e33f7864498cd42c26a9997485485
Allie Maus <aem3780@rit.edu>
2023-08-25 11:28:33 -0400

    finishing color picker exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const Color = ({color, setSelectedColor}) => {
+    return (
+      <div
+        className={color === selectedColor ? 'color selected' : color}
+        onClick={() => setSelectedColor(color)}
+       ></div>
+    )
+  }
+  
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="orange" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
